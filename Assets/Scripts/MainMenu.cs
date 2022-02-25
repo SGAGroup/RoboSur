2df637c20ad0aad313dfa0fc3b111174914f6497
ElDzaabo <eldzaabo@gmail.com>
2022-02-25 16:31:50 +0300

    У нас есть главное меню, ура
    22 ролик выкатился. Меню кнопки нажимаются. Даже есть звук нажатия.
    Надо придумать название и сделать меню похожее на что-то человеческое

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Com.sgagdr.BlackSky
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public Launcher launcher;
+
+        public void JoinMatch()
+        {
+            launcher.Join();
+        }
+        
+        public void CreateMatch()
+        {
+            launcher.Create();            
+        }   
+        
+        public void QuitGame()
+        {
+            Application.Quit();            
+        }      
+    }
+}
